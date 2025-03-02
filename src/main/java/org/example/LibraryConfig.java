5aae2fc2fb09fcc18d92d1e725bb4148a7314e40
DanekaBm <daniyabissenova2006@gmail.com>
2025-03-02 15:19:07 +0500

    Updated LibraryConfig with @Bean methods

--- src/main/java/org/example/LibraryConfig.java
+++ src/main/java/org/example/LibraryConfig.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import org.example.repository.BookRepository;
+import org.example.service.BookService;
+import org.example.service.AdvancedBookService;
+import org.example.service.SimpleBookService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +15,15 @@ public class LibraryConfig {
     @Bean
     public BookRepository bookRepository() {
         return new BookRepository();
+    }
+
+    @Bean
+    public BookService simpleBookService(BookRepository bookRepository) {
+        return new SimpleBookService(bookRepository);
+    }
+
+    @Bean
+    public BookService advancedBookService(BookRepository bookRepository) {
+        return new AdvancedBookService(bookRepository);
     }
 }
